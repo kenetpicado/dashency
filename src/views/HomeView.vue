460da4da32e01deb0cfd1de5f3e1591ae1039fe5
Kenet <kenetpicado1@gmail.com>
2024-09-14 18:27:28 -0600

    Update pre-deploy

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -4,7 +4,7 @@ import { onMounted } from 'vue'
 import useAuth from '@/composables/useAuth'
 import { useStorage } from '@vueuse/core'
 import useHome from '@/composables/useHome'
-import ApexCharts from 'apexcharts'
+// import ApexCharts from 'apexcharts'
 
 const { getProfile } = useAuth()
 const selected = useStorage('selected', 'home')
@@ -17,49 +17,49 @@ onMounted(async () => {
   await getProfile()
   await getHome()
 
-  var chart = new ApexCharts(document.getElementById('chart'), {
-    chart: {
-      type: 'area',
-      height: 650,
-      width: '98%',
-      toolbar: {
-        show: false
-      },
-      zoom: {
-        enabled: false
-      }
-    },
-    colors: ['#dc2626', '#0C1B6E'],
-    dataLabels: {
-      enabled: false
-    },
-    stroke: {
-      curve: 'smooth',
-      width: 2
-    },
-    series: [
-      {
-        name: 'Gastos',
-        data: arrayDays.map((day) => home.value.expenses.find((r) => r.day === day)?.total || 0)
-      },
-      {
-        name: 'Ingresos',
-        data: arrayDays.map((day) => home.value.incomes.find((r) => r.day === day)?.total || 0)
-      }
-    ],
-    xaxis: {
-      categories: arrayDays
-    },
-    yaxis: {
-      labels: {
-        formatter: function (value: string) {
-          return '$ ' + value.toLocaleString()
-        }
-      }
-    }
-  })
+  // var chart = new ApexCharts(document.getElementById('chart'), {
+  //   chart: {
+  //     type: 'area',
+  //     height: 650,
+  //     width: '98%',
+  //     toolbar: {
+  //       show: false
+  //     },
+  //     zoom: {
+  //       enabled: false
+  //     }
+  //   },
+  //   colors: ['#dc2626', '#0C1B6E'],
+  //   dataLabels: {
+  //     enabled: false
+  //   },
+  //   stroke: {
+  //     curve: 'smooth',
+  //     width: 2
+  //   },
+  //   series: [
+  //     {
+  //       name: 'Gastos',
+  //       data: arrayDays.map((day) => home.value.expenses.find((r) => r.day === day)?.total || 0)
+  //     },
+  //     {
+  //       name: 'Ingresos',
+  //       data: arrayDays.map((day) => home.value.incomes.find((r) => r.day === day)?.total || 0)
+  //     }
+  //   ],
+  //   xaxis: {
+  //     categories: arrayDays
+  //   },
+  //   yaxis: {
+  //     labels: {
+  //       formatter: function (value: string) {
+  //         return '$ ' + value.toLocaleString()
+  //       }
+  //     }
+  //   }
+  // })
 
-  chart.render()
+  // chart.render()
 })
 </script>
 
@@ -69,11 +69,8 @@ onMounted(async () => {
   </header>
 
   <main v-if="home.stats.length" class="grid grid-cols-4 xl:grid-cols-5 gap-4 mb-4">
-    <StatCard
-      v-for="(stat, index) in home.stats"
-      :stat="{ ...stat, value: `$ ${stat.value.toLocaleString()}` }"
-      :key="index"
-    />
+    <StatCard v-for="(stat, index) in home.stats" :stat="{ ...stat, value: `$ ${stat.value.toLocaleString()}` }"
+      :key="index" />
   </main>
 
   <div class="bg-white p-4 border rounded-lg">
