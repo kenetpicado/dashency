93f2b59a6bd8e0e9f822a66838463ec953686d72
Kenet <kenetpicado1@gmail.com>
2024-09-14 18:05:22 -0600

    Update pre-deploy

---
+++ deploy.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env sh
+
+set -e
+
+git add .
+git commit -m "Update pre-deploy"
+git push
+
+npm run build
+
+# copiar CNAME
+#cp CNAME dist/
+
+cd dist
+
+git init
+git add .
+git commit -m 'Deploy'
+
+git push -f git@github.com:kenetpicado/dashency.git master:gh-pages
+
+cd -
