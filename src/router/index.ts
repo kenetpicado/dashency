61f58bfdde3f0a2abbfbc2e12eebd9f8ecf02354
Kenet <kenetpicado1@gmail.com>
2024-09-15 18:39:50 -0600

    Refactor router history in index.ts

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHistory, createWebHashHistory } from 'vue-router'
 import DefaultLayout from '@/layouts/DefaultLayout.vue'
 import UsersView from '@/views/UsersView.vue'
 import GuestLayout from '@/layouts/GuestLayout.vue'
@@ -6,7 +6,8 @@ import { useAuthStore } from '@/stores/auth'
 import { useStorage } from '@vueuse/core'
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  // history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHashHistory(),
   routes: [
     {
       path: '/',
