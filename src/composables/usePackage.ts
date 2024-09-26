8c85c2baed6fecac43fdd578bef39e502c58a6a4
Kenet <kenetpicado1@gmail.com>
2024-09-26 17:51:35 -0600

    Refactor usePackage.ts to include updatePackage function

--- src/composables/usePackage.ts
+++ src/composables/usePackage.ts
@@ -1,9 +1,10 @@
-import type { IPackageResponse } from '@/types'
+import type { IPackage, IPackageResponse } from '@/types'
 import { usePackageStore } from '@/stores/package'
 import api from '@/config/axios'
 import { storeToRefs } from 'pinia'
 import { ref } from 'vue'
 import cleanQueryParams from '@/utils/query-params'
+import toast from '@/utils/toast'
 
 export default function usePackage() {
   const { setPackages } = usePackageStore()
@@ -37,5 +38,22 @@ export default function usePackage() {
       })
   }
 
-  return { getPackages, packages, processing, queryParams }
+  function updatePackage(data: IPackage, callback: () => void) {
+    processing.value = true
+
+    api
+      .put('/packages/' + data.id, data)
+      .then(() => {
+        callback()
+        toast.success('Paquete actualizado correctamente')
+      })
+      .catch((error) => {
+        console.log(error)
+      })
+      .finally(() => {
+        processing.value = false
+      })
+  }
+
+  return { getPackages, packages, processing, queryParams, updatePackage }
 }
--- src/views/Package/PackageView.vue
+++ src/views/Package/PackageView.vue
@@ -3,20 +3,25 @@
     <span class="font-bold text-2xl">Paquetes</span>
   </header>
 
+  <DialogForm title="Paquete" :isOpen="openModal" @onClose="resetValues">
+    <form @submit.prevent="onSubmit()">
+      <InputForm text="Cliente" name="client" v-model="form.client" required />
+      <InputForm text="Descripción" name="description" v-model="form.description" required />
+      <InputForm text="Piezas" name="pieces" v-model="form.pieces" type="number" required />
+      <InputForm text="Peso (lbs)" name="grossWeight" v-model="form.grossWeight" type="number" required />
+
+      <div class="flex justify-end gap-4">
+        <BtnSecondary @click="resetValues">Cancelar</BtnSecondary>
+        <BtnPrimary type="submit" :loading="processing">
+          Actualizar
+        </BtnPrimary>
+      </div>
+    </form>
+  </DialogForm>
+
   <div class="grid grid-cols-4 gap-4 mb-4">
-    <InputForm
-      text="Guía"
-      name="guide"
-      v-model="queryParams.guide"
-      placeholder="Número de guia"
-      type="number"
-    />
-    <InputForm
-      text="Cliente"
-      name="client"
-      v-model="queryParams.client"
-      placeholder="Nombre del cliente"
-    />
+    <InputForm text="Guía" name="guide" v-model="queryParams.guide" placeholder="Número de guia" type="number" />
+    <InputForm text="Cliente" name="client" v-model="queryParams.client" placeholder="Nombre del cliente" />
     <SelectForm text="Tipo" name="type" v-model="queryParams.type">
       <option value="">Todos</option>
       <option v-for="price in prices" :value="price.type" :key="price.id">{{ price.type }}</option>
@@ -33,19 +38,15 @@
   </div>
 
   <div v-if="packages.data.length" class="grid grid-cols-2 xl:grid-cols-3 gap-4 mb-4">
-    <PackageCard
-      v-for="(item, index) in packages.data"
-      :item="item"
-      :key="index"
-      :showIcon="false"
-    />
+    <PackageCard v-for="(item, index) in packages.data" :item="item" :key="index"
+      :showIcon="item.status === 'REGISTRADO'" :icon="IconEdit" @selectedItem="editPackage" />
   </div>
 
   <PaginationComponent :pages="packages.pages" :page="packages.current" @selected="getThisPage" />
 </template>
 
 <script setup lang="ts">
-import { onMounted } from 'vue'
+import { onMounted, ref } from 'vue'
 import usePackage from '@/composables/usePackage'
 import SelectForm from '@/components/Form/SelectForm.vue'
 import InputForm from '@/components/Form/InputForm.vue'
@@ -54,9 +55,25 @@ import PackageCard from '@/components/PackageCard.vue'
 import PaginationComponent from '@/components/PaginationComponent.vue'
 import status from '@/utils/status'
 import usePrice from '@/composables/usePrice'
+import { IconEdit } from '@tabler/icons-vue'
+import type { IPackage } from '@/types'
+import BtnSecondary from '@/components/Buttons/BtnSecondary.vue'
+import BtnPrimary from '@/components/Buttons/BtnPrimary.vue'
+import DialogForm from '@/components/Form/DialogForm.vue'
 
-const { getPackages, packages, queryParams } = usePackage()
+const { getPackages, packages, queryParams, processing, updatePackage } = usePackage()
 const { prices, getPrices } = usePrice()
+
+const openModal = ref<boolean>(false)
+
+const form = ref<IPackage>({
+  guide: '',
+  client: '',
+  description: '',
+  pieces: 0,
+  grossWeight: 0,
+  entryDate: '',
+})
 
 onMounted(() => {
   getPackages()
@@ -67,5 +84,29 @@ watchDebounced(queryParams.value, () => getPackages(), { debounce: 700, maxWait:
 
 function getThisPage(page: number) {
   queryParams.value.page = page
+}
+
+function editPackage(item: IPackage) {
+  form.value = { ...item }
+  openModal.value = true
+}
+
+function resetValues() {
+  form.value = {
+    guide: '',
+    client: '',
+    description: '',
+    pieces: 0,
+    grossWeight: 0,
+    entryDate: '',
+  }
+  openModal.value = false
+}
+
+function onSubmit() {
+  updatePackage(form.value, () => {
+    resetValues()
+    getPackages()
+  })
 }
 </script>
